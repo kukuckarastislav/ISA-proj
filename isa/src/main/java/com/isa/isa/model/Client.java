a4a50f71f64679c6d5b7d817dc590f9c65b49c2c
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2021-11-07 16:47:28 +0100

    init: added model class for client

---
+++ isa/src/main/java/com/isa/isa/model/Client.java
@@ -0,0 +1,126 @@
+package com.isa.isa.model;
+
+public class Client {
+	
+	private int id;
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String addres;
+	private String city;
+	private String country;
+	private String phoneNumber;
+	
+	
+	public Client() {
+
+	}
+
+
+	public Client(int id, String email, String password, String firstName, String lastName, String addres, String city,
+			String country, String phoneNumber) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.addres = addres;
+		this.city = city;
+		this.country = country;
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getAddres() {
+		return addres;
+	}
+
+
+	public void setAddres(String addres) {
+		this.addres = addres;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+}
