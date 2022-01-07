09796728a8972f924fa9f44b0b096c167a2d4796
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2022-01-07 21:36:07 +0100

    feat: sliding images adventure page

--- frontend/isa-frontend/src/instructor/AdventureView.vue
+++ frontend/isa-frontend/src/instructor/AdventureView.vue
@@ -2,11 +2,45 @@
     <div style="height: 60px;"></div>
     <div class="container">
         <div class="row">
-            <div class="col-sm-3">
-                aa
+            <div class="col-sm-6">
+
+               <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                 
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label=""></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" class="active" aria-current="true" aria-label=""></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" class="active" aria-current="true" aria-label=""></button>
+                 
+                 </div>
+                <div class="carousel-inner">
+
+                   
+                    <div class="carousel-item active">
+                      
+                    </div>
+                  
+
+
+                  <div v-for="image in adventure.images" :key="image">
+                    <div class="carousel-item">
+                      <img v-bind:src="setImg(image)" class="d-block w-100">
+                    </div>
+                  </div>
+
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+
             </div>
-            <div class="col-sm-9">
-                coool
+            <div class="col-sm-6">
+
             </div>
         </div>
     </div>
@@ -22,7 +56,8 @@ export default {
   },
   data: function(){
     return {
-        adventureName: ""
+        adventureName: "",
+        adventure: {}
     }
   },
   mounted: function(){
@@ -33,11 +68,13 @@ export default {
     loadData: function(){
        axios.defaults.headers.common["Authorization"] = "Bearer " + window.sessionStorage.getItem("jwt");  
         axios.get('http://localhost:8180/api/adventure/byinstructor/'+encodeURIComponent(this.adventureName)).then(resp => {
-            this.adventures = resp.data;
-               this.sortAdventures();
+            this.adventure = resp.data;
             console.log(resp.data);
         });
-    }
+    },
+    setImg: function(image){
+      return 'http://localhost:8180/'+image.path;
+    },
   }
 }
 </script>
