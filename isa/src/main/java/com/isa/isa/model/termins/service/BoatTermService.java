3e48f2b42fdf45ba3571def6a4c35a19fa3e0e7e
mihajlo-ostojic <ostojic.ra134.2018@uns.ac.rs>
2022-05-29 18:37:30 +0200

    feat: added services for adding new boat terms

--- isa/src/main/java/com/isa/isa/model/termins/service/BoatTermService.java
+++ isa/src/main/java/com/isa/isa/model/termins/service/BoatTermService.java
@@ -1,22 +1,30 @@
 package com.isa.isa.model.termins.service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.isa.isa.model.Boat;
 import com.isa.isa.model.termins.DTO.BoatTermsDTO;
-import com.isa.isa.model.termins.DTO.CottageTermsDTO;
+import com.isa.isa.model.termins.DTO.NewBoatTermDto;
+import com.isa.isa.model.termins.model.BoatReservations;
 import com.isa.isa.model.termins.model.BoatTerms;
-import com.isa.isa.model.termins.model.CottageTerms;
 import com.isa.isa.model.termins.model.TermAvailability;
+import com.isa.isa.model.termins.repository.BoatReservationRepository;
 import com.isa.isa.model.termins.repository.BoatTermRepository;
+import com.isa.isa.repository.BoatRepository;
 
 @Service
 public class BoatTermService {
 
 	@Autowired
     private BoatTermRepository boatTermRepository;
+	@Autowired
+	private BoatRepository boatRepository;
+	@Autowired
+	private BoatReservationRepository boatReservationRepository;
 	
 	public Boolean isBoatFree(BoatTermsDTO dto) {
 		ArrayList<BoatTerms> terms = (ArrayList<BoatTerms>) boatTermRepository.findAllByBoatId(dto.getId());
@@ -29,4 +37,50 @@ public class BoatTermService {
 		}
 		return retVal;
 	}
+	
+	public String defineNewTermForBoat(NewBoatTermDto newBoatTermDto) {
+	        BoatTerms newTerm = new BoatTerms(newBoatTermDto.getStartTime(), newBoatTermDto.getEndTime());
+	        Boat boat;
+	        
+	        boat = boatRepository.getById(newBoatTermDto.getBoatId());
+	        newTerm.setBoat(boat);
+	        newTerm.setStartTime(newBoatTermDto.getStartTime());
+	        newTerm.setEndTime(newBoatTermDto.getEndTime());
+	        newTerm.setTermAvailability(newBoatTermDto.getTermAvailability());
+	        
+	        if(isBoatTermOverlapedWithExistingBoatTerms(getAllTermsForBoat(boat.getId()),newTerm))
+	        {
+	        	return "Cannot define new term because it is overlaped with another term.";
+	        }
+	        if(isBoatTermOverlapedWithExistingBoatReservation(boatReservationRepository.findAllByBoatId(boat.getId()),newTerm))
+	        {
+	        	return "Cannot define new term because it is overlaped with another reservation.";
+	        }
+	        
+	        boatTermRepository.save(newTerm);
+	        
+	        return "Boat term successfully defined.";
+	 }
+	
+	public List<BoatTerms> getAllTermsForBoat(int boatId) {
+		List<BoatTerms> allBoatTerms = boatTermRepository.findAllByBoatId(boatId);
+        return allBoatTerms;
+    }
+	
+	public boolean isBoatTermOverlapedWithExistingBoatTerms(List<BoatTerms> boatTerms, BoatTerms newBoatTerm) {
+	      for (BoatTerms term : boatTerms)
+	         if (term.getStartTime().isBefore(newBoatTerm.getStartTime()) && term.getEndTime().isAfter(newBoatTerm.getEndTime()))
+	            return true;
+
+	      return false;
+	}
+	
+	public boolean isBoatTermOverlapedWithExistingBoatReservation(List<BoatReservations> boatReservations, BoatTerms newBoatTerm) {
+	      for (BoatReservations reservation : boatReservations)
+	         if (reservation.getStartTime().isBefore(newBoatTerm.getStartTime()) && reservation.getEndTime().isAfter(newBoatTerm.getEndTime()))
+	            return true;
+
+	      return false;
+	}
+	
 }
