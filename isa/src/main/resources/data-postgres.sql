7ebf1cc868732f2333c8697ca925d8209ac584c0
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2022-06-04 20:40:45 +0200

    fix: sql script coordinates

--- isa/src/main/resources/data-postgres.sql
+++ isa/src/main/resources/data-postgres.sql
@@ -35,7 +35,7 @@ values ('isaprojectftn+admin@gmail.com','$2a$10$3xqAvzqpGKgBJrzIzpBQf.lm3RaR3R0.
 insert into user_role (user_id, role_id)
 values (1, 1);
 insert into admin (id, city, country, latitude, longitude, number, street, email, password, first_name, last_name, phone_number)
-values (1, 'Novi Sad', 'Serbia', 34, 54, 10, 'Bulevar evrope', 'isaprojectftn+admin@gmail.com', '$2a$10$3xqAvzqpGKgBJrzIzpBQf.lm3RaR3R0.SAZsE6JovkCKv.9.9QgPG', 'Admin', 'Admin', '030345');
+values (1, 'Novi Sad', 'Serbia', 45.2471591374095, 19.818258855810267, 10, 'Bulevar evrope', 'isaprojectftn+admin@gmail.com', '$2a$10$3xqAvzqpGKgBJrzIzpBQf.lm3RaR3R0.SAZsE6JovkCKv.9.9QgPG', 'Admin', 'Admin', '030345');
 
 -- Admin2
 insert into USERS (username, password, last_password_reset_date, enabled, approved,admin_ready)
@@ -43,7 +43,7 @@ values ('isaprojectftn+administrator@gmail.com','$2a$10$OnaRYDYNzDYjx6iju8sK9eGM
 insert into user_role (user_id, role_id)
 values (2, 1);
 insert into admin (id, city, country, latitude, longitude, number, street, email, password, first_name, last_name, phone_number)
-values (2, 'Beograd', 'Serbia', 35, 564, 13, 'Bulevar evrope', 'isaprojectftn+administrator@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'Admin', 'Admin', '030345');
+values (2, 'Beograd', 'Serbia', 44.77609210891258, 20.549114382934537, 13, 'Bulevar evrope', 'isaprojectftn+administrator@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'Admin', 'Admin', '030345');
 
 -- Client Mirija
 insert into USERS (username, password, last_password_reset_date, enabled, approved,admin_ready)
@@ -51,7 +51,7 @@ values ('isaprojectftn+marijaC@gmail.com','$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGS
 insert into user_role (user_id, role_id)
 values (3, 3);
 insert into client(id, score, loyalty_type, city, country, latitude, longitude, number, street, email, password, first_name, last_name, phone_number)
-values (3, 0, 0, 'Novi Sad', 'Serbia', 11, 13, 33, 'Zmaj Jovina', 'isaprojectftn+marijaC@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'Marija', 'Maric', '030348');
+values (3, 0, 0, 'Novi Sad', 'Serbia', 45.257120944214186, 19.847844898140064, 33, 'Zmaj Jovina', 'isaprojectftn+marijaC@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'Marija', 'Maric', '030348');
 
 -- Cottage owner milan
 insert into USERS (username, password, last_password_reset_date, enabled, approved,admin_ready)
@@ -59,9 +59,9 @@ values ('isaprojectftn+milanCO@gmail.com','$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGS
 insert into user_role (user_id, role_id)
 values (4, 4);
 insert into cottage_owner (id, score, loyalty_type, city, country, latitude, longitude, number, street, email, password, first_name, last_name, phone_number, registration_motivation, average_grade)
-values (4,0,0, 'Novi Sad', 'Serbia', 34, 54, 10, 'Bulevar evrope', 'isaprojectftn+milanCO@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'milan', 'milanovic', '030345', 'imao sam zelju', 0);
+values (4,0,0, 'Novi Sad', 'Serbia', 45.247249780971536, 19.818205211634332, 10, 'Bulevar evrope', 'isaprojectftn+milanCO@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'milan', 'milanovic', '030345', 'imao sam zelju', 0);
 insert into cottage (name, description, behaviour_rules, price,price_type, owner_id, city, country, latitude, longitude, number, street, average_grade, capacity)
-values ('Stan', 'Lep stan', 'zabranjeno glasna muzika', 1000,'PER_DAY', 4, 'Zlatibor', 'Serbia', 11, 13, 33, 'Cara Dusana', 0, 5);
+values ('Stan', 'Lep stan', 'zabranjeno glasna muzika', 1000,'PER_DAY', 4, 'Zlatibor', 'Serbia', 43.73244496341901, 19.69687467198646, 33, 'Cara Dusana', 0, 5);
 
 
 -- Cottage owner petar
@@ -70,9 +70,9 @@ values ('isaprojectftn+petarCO@gmail.com','$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGS
 insert into user_role (user_id, role_id)
 values (5, 4);
 insert into cottage_owner (id, score, loyalty_type, city, country, latitude, longitude, number, street, email, password, first_name, last_name, phone_number, registration_motivation, average_grade)
-values (5,0,0, 'Beograd', 'Serbia', 14, 24, 35, 'Knez', 'isaprojectftn+petarCO@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'petar', 'petrovic', '030345', 'imao sam zelju da iznamljujem sobe', 0);
+values (5,0,0, 'Beograd', 'Serbia', 44.817766374150516, 20.456455013470055, 35, 'Knez', 'isaprojectftn+petarCO@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'petar', 'petrovic', '030345', 'imao sam zelju da iznamljujem sobe', 0);
 insert into cottage (name, description, behaviour_rules, price, price_type, owner_id, city, country, latitude, longitude, number, street, average_grade, capacity)
-values ('Kuca', 'mala kuca', 'hrana', 500, 'PER_DAY', 5, 'Kopaonik', 'Serbia', 11, 13, 33, 'Cara Lazara', 0, 4);
+values ('Kuca', 'mala kuca', 'hrana', 500, 'PER_DAY', 5, 'Kopaonik', 'Serbia', 43.2760831163471, 20.76000198459797, 33, 'Cara Lazara', 0, 4);
 
 insert into cottage_terms(cottage_id, term_availability, start_time, end_time)
 values (2, 'AVAILABILE', '2022-04-05T8:00:06.235098700', '2022-07-25T8:00:06.235098700');
@@ -83,7 +83,7 @@ values (1,3);
 insert into cottage_reservation_additional_services(cottage_reservation_id, additional_service_id)
 values (1,4);
 insert into cottage_fast_reservation(city, country, latitude, longitude, number, street, max_number_of_people, price, start_time, end_time, cottage_id)
-values ('Bratislava', 'Slovakia', 14, 24, 35, 'Miskova', 4, 100, '2022-07-11T14:00:06.235098700', '2022-07-12T16:00:06.235098700', 2);
+values ('Bratislava', 'Slovakia', 48.1524021968794, 17.102130827048573, 35, 'Miskova', 4, 100, '2022-07-11T14:00:06.235098700', '2022-07-12T16:00:06.235098700', 2);
 insert into cottage_fast_res_history(status_of_fast_reservation, client_id, cottage_fast_res_id, is_revised, is_complained_of)
 values ('TAKEN', 3, 1, false, false);
 
@@ -95,9 +95,9 @@ values ('isaprojectftn+stefanI@gmail.com','$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGS
 insert into user_role (user_id, role_id)
 values (6, 5);
 insert into instructor (id, score, loyalty_type, city, country, latitude, longitude, number, street, email, password, first_name, last_name, phone_number, biography, registration_motivation, average_grade)
-values (6, 0,0, 'Bratislava', 'Slovakia', 14, 24, 35, 'Miskova', 'isaprojectftn+stefanI@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'Stefan', 'Stefanovic', '030345', 'moja biografija, ja sam super lik', 'Hoho instruktor',0);
+values (6, 0,0, 'Bratislava', 'Slovakia', 48.1524021968794, 17.102130827048573, 35, 'Miskova', 'isaprojectftn+stefanI@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'Stefan', 'Stefanovic', '030345', 'moja biografija, ja sam super lik', 'Hoho instruktor',0);
 insert into adventure (name, city, country, latitude, longitude, number, street, price, price_type, description, biography, max_number_of_people, behaviour_rules, reservation_cancellation_conditions, instructor_id, average_grade)
-values ('Penjanje na planinu', 'Bratislava', 'Slovakia', 14, 24, 35, 'Miskova', 5,'PER_HOUR','Pecacemo Ribe', 'moja biografija, ja sam super lik', 4, 'Nema skakanja u vodu', 'FREE', 6, 0);
+values ('Penjanje na planinu', 'Bratislava', 'Slovakia', 48.1524021968794, 17.102130827048573, 35, 'Miskova', 5,'PER_HOUR','Pecacemo Ribe', 'moja biografija, ja sam super lik', 4, 'Nema skakanja u vodu', 'FREE', 6, 0);
 insert into adventure_additional_services(adventure_id, additional_service_id)
 values (1,1);
 insert into adventure_additional_services(adventure_id, additional_service_id)
@@ -117,7 +117,7 @@ values (1, 2);
 insert into adventure_images (adventure_id, images_id)
 values (1, 3);
 insert into adventure (name, city, country, latitude, longitude, number, street, price, price_type, description, biography, max_number_of_people, behaviour_rules, reservation_cancellation_conditions, instructor_id, average_grade)
-values ('Krstarenje', 'Bratislava', 'Slovakia', 14, 24, 35, 'Miskova', 5,'PER_HOUR' ,'Pecacemo Ribe', 'moja biografija, ja sam super lik', 4, 'Nema skakanja u vodu', 'FREE', 6, 0);
+values ('Krstarenje', 'Bratislava', 'Slovakia', 48.1524021968794, 17.102130827048573, 35, 'Miskova', 5,'PER_HOUR' ,'Pecacemo Ribe', 'moja biografija, ja sam super lik', 4, 'Nema skakanja u vodu', 'FREE', 6, 0);
 insert into adventure_additional_services(adventure_id, additional_service_id)
 values (2,1);
 insert into entity_image (name, path)
@@ -143,7 +143,7 @@ values (1,3);
 insert into instructor_reservation_additional_services(instructor_reservation_id, additional_service_id)
 values (1,4);
 insert into instructor_fast_reservation(city, country, latitude, longitude, number, street, max_number_of_people, price, start_time, end_time, adventure_id, instructor_username)
-values ('Bratislava', 'Slovakia', 14, 24, 35, 'Miskova', 4, 5, '2022-07-11T14:00:06.235098700', '2022-07-11T16:00:06.235098700', 2, 'isaprojectftn+stefanI@gmail.com');
+values ('Bratislava', 'Slovakia', 48.1524021968794, 17.102130827048573, 35, 'Miskova', 4, 5, '2022-07-11T14:00:06.235098700', '2022-07-11T16:00:06.235098700', 2, 'isaprojectftn+stefanI@gmail.com');
 insert into ins_fast_res_history(status_of_fast_reservation, client_id, ins_fast_res_id, is_revised, is_complained_of)
 values ('CANCELLED', 3, 1, false, false);
 insert into instructor_fast_reservation_additional_services(instructor_fast_reservation_id,additional_service_id)
@@ -169,9 +169,9 @@ values ('isaprojectftn+nikolaBO@gmail.com','$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicG
 insert into user_role (user_id, role_id)
 values (7, 2);
 insert into boat_owner (id, score, loyalty_type, city, country, latitude, longitude, number, street, email, password, first_name, last_name, phone_number, registration_motivation, average_grade)
-values (7, 0,0,'Zrenjanin', 'Serbia', 11, 13, 33, 'Zmaj Jovina', 'isaprojectftn+nikolaBO@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'Nikola', 'Nikolic', '030346', 'imao sam zelju', 0);
+values (7, 0,0,'Zrenjanin', 'Serbia', 45.37603402791083, 20.393630711637407, 33, 'Zmaj Jovina', 'isaprojectftn+nikolaBO@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'Nikola', 'Nikolic', '030346', 'imao sam zelju', 0);
 insert into boat (city, country, latitude, longitude, number, street,price, price_type, behaviour_rules, capacity, engine_number, engine_power, length, max_speed, name, promotional_description, type, owner_id,reservation_cancellation_conditions,average_grade)
-values ('Beograd', 'Serbia', 11.0, 13.0, 33, 'Knez Mihajlova', 10, 'PER_HOUR', 'Nema brze voznje', 3, '123', 100, 50, 70, 'Laser 16', 'Najbolji brod', 'Jedrilica', 7, 'FREE',0);
+values ('Beograd', 'Serbia', 44.81773309538738, 20.456885226964513, 33, 'Knez Mihajlova', 10, 'PER_HOUR', 'Nema brze voznje', 3, '123', 100, 50, 70, 'Laser 16', 'Najbolji brod', 'Jedrilica', 7, 'FREE',0);
 insert into entity_image (name, path)
 values ('img1.jpg', 'images/BoatOwners/isaprojectftn+nikolaBO@gmail.com/Laser_16/img1.jpg');
 insert into boat_images(boat_id,images_id)
@@ -201,7 +201,7 @@ values (2,4);
 insert into boat_terms(boat_id, term_availability, start_time, end_time)
 values (2, 'AVAILABILE', '2022-04-05T8:00:06.235098700', '2022-07-25T8:00:06.235098700');
 insert into boat_fast_reservation(city, country, latitude, longitude, number, street, max_number_of_people, price, start_time, end_time, boat_id)
-values ('Novi Sad', 'Serbia', 14, 24, 35, 'Vidovdanska', 4, 20, '2022-07-11T14:00:06.235098700', '2022-07-11T16:00:06.235098700', 2);
+values ('Novi Sad', 'Serbia', 45.279115064645225, 19.833173111634995, 35, 'Vidovdanska', 4, 20, '2022-07-11T14:00:06.235098700', '2022-07-11T16:00:06.235098700', 2);
 insert into boat_fast_reservation_additional_services(boat_fast_reservation_id,additional_service_id)
 values (1,1);
 insert into boat_fast_reservation_additional_services(boat_fast_reservation_id,additional_service_id)
@@ -287,9 +287,9 @@ values ('isaprojectftn+alexI@gmail.com','$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnS
 insert into user_role (user_id, role_id)
 values (8, 5);
 insert into instructor (id, score, loyalty_type, city, country, latitude, longitude, number, street, email, password, first_name, last_name, phone_number, biography, registration_motivation, average_grade)
-values (8, 0,0, 'Novi Sad', 'Srbija', 14, 24, 35, 'Bulevar oslobodjenja', 'isaprojectftn+alexI@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'Alexandar', 'Lux', '030345', 'Odlican instrutktor, naucicu te svim cakama i vestinama, jedva cekam', 'To mi je San od malena, da ucim druge ',0);
+values (8, 0,0, 'Novi Sad', 'Srbija', 45.26050908726751, 19.832872269305035, 35, 'Bulevar oslobodjenja', 'isaprojectftn+alexI@gmail.com', '$2a$10$OnaRYDYNzDYjx6iju8sK9eGMkmicGSnSPETdBqLKLYVRjyJSw5/QC', 'Alexandar', 'Lux', '030345', 'Odlican instrutktor, naucicu te svim cakama i vestinama, jedva cekam', 'To mi je San od malena, da ucim druge ',0);
 insert into adventure (name, city, country, latitude, longitude, number, street, price, price_type, description, biography, max_number_of_people, behaviour_rules, reservation_cancellation_conditions, instructor_id, average_grade)
-values ('Obilazak piramide', 'Aleksandrija', 'Egipt', 14, 24, 35, 'piro', 5,'PER_HOUR','Penjamo se na piramidu, budimo faraona', 'Odlican instrutktor, naucicu te svim cakama i vestinama, jedva cekam', 4, 'Nema kradje zlata, i cupova iz piramide', 'FREE', 8, 0);
+values ('Obilazak piramide', 'Aleksandrija', 'Egipt', 31.224646848528113, 29.951699984354544, 35, 'piro', 5,'PER_HOUR','Penjamo se na piramidu, budimo faraona', 'Odlican instrutktor, naucicu te svim cakama i vestinama, jedva cekam', 4, 'Nema kradje zlata, i cupova iz piramide', 'FREE', 8, 0);
 insert into adventure_additional_services(adventure_id, additional_service_id)
 values (3,1);
 insert into adventure_additional_services(adventure_id, additional_service_id)
@@ -319,7 +319,7 @@ values (2,3);
 insert into instructor_reservation_additional_services(instructor_reservation_id, additional_service_id)
 values (2,4);
 --insert into instructor_fast_reservation(city, country, latitude, longitude, number, street, max_number_of_people, price, start_time, end_time, adventure_id, instructor_username)
---values ('Bratislava', 'Slovakia', 14, 24, 35, 'Miskova', 4, 5, '2022-07-11T14:00:06.235098700', '2022-07-11T16:00:06.235098700', 2, 'isaprojectftn+stefanI@gmail.com');
+--values ('Bratislava', 'Slovakia', 48.1524021968794, 17.102130827048573, 35, 'Miskova', 4, 5, '2022-07-11T14:00:06.235098700', '2022-07-11T16:00:06.235098700', 2, 'isaprojectftn+stefanI@gmail.com');
 --insert into ins_fast_res_history(status_of_fast_reservation, client_id, ins_fast_res_id, is_revised, is_complained_of)
 --values ('CANCELLED', 3, 1, false, false);
 --insert into instructor_fast_reservation_additional_services(instructor_fast_reservation_id,additional_service_id)
