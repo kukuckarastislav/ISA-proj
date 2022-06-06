a88b1852c74acd48c47e045e9d885dfa0c098fbd
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2022-06-06 23:04:11 +0200

    feat: transaction pdf s3

--- isa/src/main/java/com/isa/isa/model/loyalty/LoyaltyService.java
+++ isa/src/main/java/com/isa/isa/model/loyalty/LoyaltyService.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class LoyaltyService {
 
